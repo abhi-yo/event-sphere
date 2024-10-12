cf73a54d3d385c01cc7df80b560ce02400db7d14
abhi-yo <akshatm.com@gmail.com>
2024-10-12 13:37:23 +0530

    chore: move to classic markers

--- components/EventMap.tsx
+++ components/EventMap.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { Loader } from "@googlemaps/js-api-loader";
 import { useEffect, useRef, useState } from "react";
-
 interface Event {
   _id: string;
   title: string;
@@ -10,35 +9,27 @@ interface Event {
     coordinates: [number, number];
   };
 }
-
 interface EventMapProps {
   events: Event[];
   onLocationSelect?: (lat: number, lng: number) => void;
   selectedEvent?: Event | null;
 }
-
 export default function EventMap({ events, onLocationSelect, selectedEvent }: EventMapProps) {
   const mapRef = useRef<HTMLDivElement>(null);
   const [map, setMap] = useState<google.maps.Map | null>(null);
   const [markers, setMarkers] = useState<google.maps.Marker[]>([]);
   const [selectionMarker, setSelectionMarker] = useState<google.maps.Marker | null>(null);
-
   useEffect(() => {
     const loader = new Loader({
       apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
       version: "weekly",
     });
-
     loader.load().then(() => {
       if (mapRef.current && !map) {
-        // const kolkataCoordinates = { lat: 22.5726, lng: 88.3639 };
-        // const newMap = new google.maps.Map(mapRef.current, {
-        //   center: kolkataCoordinates,
-        //   zoom: 9,
-        // });
+        const kolkataCoordinates = { lat: 22.5726, lng: 88.3639 };
         const newMap = new google.maps.Map(mapRef.current, {
-          center: { lat: 20, lng: 0 },
-          zoom: 2,
+          center: kolkataCoordinates,
+          zoom: 9,
         });
         setMap(newMap);
         if (onLocationSelect) {
@@ -53,8 +44,7 @@ export default function EventMap({ events, onLocationSelect, selectedEvent }: Ev
         }
       }
     });
-  }, [onLocationSelect, map]);
-
+  }, [onLocationSelect]);
   useEffect(() => {
     if (map) {
       markers.forEach((marker) => marker.setMap(null));
@@ -67,22 +57,19 @@ export default function EventMap({ events, onLocationSelect, selectedEvent }: Ev
           map: map,
           title: event.title,
         });
-
         marker.addListener("click", () => {
-          const url = `https://www.google.com/maps/dir/?api=1&destination=${event.location.coordinates[1]},${event.location.coordinates[0]}`;
-          window.open(url, "_blank");
+          map.setCenter(marker.getPosition()!);
+          map.setZoom(12);
         });
-
         return marker;
       });
       setMarkers(newMarkers);
     }
   }, [map, events]);
-
   useEffect(() => {
     if (map && selectedEvent) {
       const selectedMarker = markers.find(
-        (marker) => marker.getTitle() === selectedEvent.title
+        marker => marker.getTitle() === selectedEvent.title
       );
       if (selectedMarker) {
         map.setCenter(selectedMarker.getPosition()!);
@@ -90,7 +77,6 @@ export default function EventMap({ events, onLocationSelect, selectedEvent }: Ev
       }
     }
   }, [selectedEvent, map, markers]);
-
   const placeSelectionMarker = (latLng: google.maps.LatLng) => {
     if (selectionMarker) {
       selectionMarker.setMap(null);
@@ -102,6 +88,5 @@ export default function EventMap({ events, onLocationSelect, selectedEvent }: Ev
     });
     setSelectionMarker(newMarker);
   };
-
   return <div ref={mapRef} className="w-full h-[400px]" />;
 }
